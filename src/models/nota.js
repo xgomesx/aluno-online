690c817136536764cbdc560edbd7209cbfdb018c
xgomesx <104596996+xgomesx@users.noreply.github.com>
2023-05-31 13:26:24 -0300

    notas-att
    Atualização do range das notas..

--- src/models/nota.js
+++ src/models/nota.js
@@ -8,8 +8,8 @@ class Nota {
 
     mediaFinal() {
         return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a1 + 0.6 * this.a2,
+            0.4 * this.a1 + 0.6 * this.a3,
             0.4 * this.a3 + 0.6 * this.a2
         );
     }
@@ -17,22 +17,22 @@ class Nota {
     mediaCA() {
         // Hotfix Função que mostra notas com medias escritas 
         var m = Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a1 + 0.6 * this.a2,
+            0.4 * this.a1 + 0.6 * this.a3,
             0.4 * this.a3 + 0.6 * this.a2
         );
-        if (m<=10) {
-            if (m>=9) {
+        if (m <= 10) {
+            if (m >= 9) {
                 return "SS";
-            } else if(m >= 7 && m<9) {
+            } else if (m >= 7 && m < 9) {
                 return "MS";
-            } else if(m >=5 && m<7){
+            } else if (m >= 5 && m < 7) {
                 return "MM";
-            } else{
-                if(m>2){return "MI";}
-                return "II";
+            } else {
+                if (m >= 3) { return "MI"; } else if (m >= 0.1 && m < 3) { return "II"; }
+                return "SR";
             }
-            
+
         } else {
             return "erro na matrix de notas"
         }
